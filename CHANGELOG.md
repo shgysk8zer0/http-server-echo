4f298a078f00db4542c234094065a16bce4632a4
Chris Zuber <shgysk8zer0@gmail.com>
2025-02-14 13:45:27 -0800

    Use `getAll()` when outputting search params

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,11 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [v1.0.3] - 2025-02-14
+
+### Changed
+- Use `getAll()` when outputting search params
+
 ## [v1.0.2] - 2025-02-13
 
 ### Changed
--- echo.js
+++ echo.js
@@ -1,16 +1,29 @@
-export default async function(req, { searchParams, ... context}) {
+/**
+ *
+ * @param {Request} request
+ * @param {object} context
+ * @param {URLSearchParams} context.searchParams
+ * @returns {Promise<Response>}
+ */
+export default async function(request, { searchParams, ...context }) {
 	return Response.json({
-		url: req.url,
-		method: req.method,
-		headers: Object.fromEntries(req.headers),
-		mode: req.mode,
-		destination: req.destination,
-		referrer: req.referrer,
-		credentials: req.credentials,
-		cache: req.cache,
-		priority: req.priority,
-		body: req.body instanceof ReadableStream ? await req.text() : null,
-		signal: { aborted: req.signal.aborted, reason: req.signal.reason ?? null },
-		context: { searchParams: Object.fromEntries(searchParams), ...context },
+		url: request.url,
+		method: request.method,
+		headers: Object.fromEntries(request.headers),
+		mode: request.mode,
+		destination: request.destination,
+		referrer: request.referrer,
+		credentials: request.credentials,
+		cache: request.cache,
+		priority: request.priority,
+		body: request.body instanceof ReadableStream ? Array.from(await request.bytes()) : null,
+		signal: { aborted: request.signal.aborted, reason: request.signal.reason ?? null },
+		context: {
+			searchParams: Object.fromEntries(Array.from(
+				searchParams.keys(),
+				key => [key, searchParams.getAll(key)]
+			)),
+			...context
+		},
 	});
 }
